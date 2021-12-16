f9ac2d2dba173c9c75f3a56c10129c7b5039de50
Connery Noble <connery@codezero.io>
2021-12-16 11:12:25 -0800

    czctl 1.2.2 (#4)

--- Formula/czctl.rb
+++ Formula/czctl.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Czctl < Formula
   desc "Develop, debug, deploy using CodeZero"
   homepage "https://codezero.io/"
-  url "https://registry.npmjs.org/@c6o/cli/-/cli-1.2.1.tgz"
-  sha256 "4e0546fd732eb10dbf8398d561af08010bbf8e75b40ccbe6fdd8af9f05c9ffe1"
+  url "https://registry.npmjs.org/@c6o/cli/-/cli-1.2.2.tgz"
+  sha256 "d43e39326f90772d57a03b5c6d08f28f3aa777acb5e4740ae112c85aa09963cb"
 
   depends_on "node"
 
