64e333ccb546f1d19182d345895b7528a412bda6
Connery Noble <connery@qwerti.ca>
2022-03-09 14:26:03 -0800

    fix(canary): minor typo in canary, duplicate sha

--- Casks/codezero-canary.rb
+++ Casks/codezero-canary.rb
@@ -1,6 +1,5 @@
 cask 'codezero-canary' do
     arch = Hardware::CPU.intel? ? "x64" : "arm64"
-    sha256 "d9bb3fb4c275336512c3c52c9db333a75ca2b2f9919bf271a86ae91eae160d9f"
   
     version "1.4.0-alpha.2"
     sha256 :no_check
