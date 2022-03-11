15eed92def0fca7a0cd4a732ca7acf8fbc40e4fe
Connery Noble <connery@qwerti.ca>
2022-03-10 18:56:03 -0800

    fix(casks): remove conflicts_with formulas

--- Casks/codezero-canary.rb
+++ Casks/codezero-canary.rb
@@ -4,10 +4,7 @@ cask 'codezero-canary' do
     version "1.4.0-alpha.2"
     sha256 :no_check
 
-    conflicts_with [
-        "c6o/tools/czctl-canary",
-        "c6o/tools/czctl"
-    ], cask: "c6o/tools/codezero"
+    conflicts_with cask: "c6o/tools/codezero"
   
     url "https://releases.codezero.io/#{version}/CodeZero-darwin-#{arch}.pkg"
     name 'CodeZero'
